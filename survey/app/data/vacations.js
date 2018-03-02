c95d5f69ed7a3256cd3b273fce3ec830df62275f
tke288 <31257759+tke288@users.noreply.github.com>
2018-03-01 18:33:23 -0600

    Add files via upload

---
+++ survey/app/data/vacations.js
@@ -0,0 +1,36 @@
+var vacations = [
+    {
+        "name": "beach1",
+        "answers": [
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "mountain2",
+        "answers": [
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "washtington d.c.",
+        "answers": [
+            3,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "mexico",
+        "answers": [
+            2,
+            1,
+            1
+        ]
+    }
+]
+
+module.exports = vacations;
