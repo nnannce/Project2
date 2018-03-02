992d254eb13f0f5fb3797735202c9c8dc5674b82
tke288 <31257759+tke288@users.noreply.github.com>
2018-03-02 15:18:49 -0600

    Edited results to show a photo and description

--- survey/app/data/vacations.js
+++ survey/app/data/vacations.js
@@ -1,6 +1,7 @@
 var vacations = [
     {
         "name": "beach1",
+        "photo": "http://static.asiawebdirect.com/m/phuket/portals/phuket-com/homepage/yourguide/romantic/beaches/pagePropertiesImage/phuket-romantic-beaches.jpg",
         "answers": [
             1,
             3,
@@ -9,6 +10,7 @@ var vacations = [
     },
     {
         "name": "mountain2",
+        "photo": "https://media-cdn.tripadvisor.com/media/photo-s/01/58/cb/8c/red-mountain-resort-lodging.jpg",
         "answers": [
             1,
             1,
@@ -17,6 +19,7 @@ var vacations = [
     },
     {
         "name": "washtington d.c.",
+        "photo": "https://fthmb.tqn.com/SJ7ydb7TT-3A0OVYr7Z3V9uHO4o=/960x0/filters:no_upscale()/capitol-building-170402241-58ddb7bf5f9b58468374c174.jpg",
         "answers": [
             3,
             2,
@@ -25,6 +28,7 @@ var vacations = [
     },
     {
         "name": "mexico",
+        "photo": "https://www.nationalgeographic.com/content/dam/travel/2016-digital/top-10-mexico-city/metropolitan-cathedral-zocalo-mexico-city.JPG",
         "answers": [
             2,
             1,
