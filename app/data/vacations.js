829d9158dfd8947b6fa2a131c3ec5fedda697de6
tke288 <31257759+tke288@users.noreply.github.com>
2018-03-03 11:16:05 -0600

    Questions populating

---
+++ app/data/vacations.js
@@ -0,0 +1,40 @@
+var vacations = [
+    {
+        "name": "beach1",
+        "photo": "http://static.asiawebdirect.com/m/phuket/portals/phuket-com/homepage/yourguide/romantic/beaches/pagePropertiesImage/phuket-romantic-beaches.jpg",
+        "answers": [
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "mountain2",
+        "photo": "https://media-cdn.tripadvisor.com/media/photo-s/01/58/cb/8c/red-mountain-resort-lodging.jpg",
+        "answers": [
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        "name": "washtington d.c.",
+        "photo": "https://fthmb.tqn.com/SJ7ydb7TT-3A0OVYr7Z3V9uHO4o=/960x0/filters:no_upscale()/capitol-building-170402241-58ddb7bf5f9b58468374c174.jpg",
+        "answers": [
+            3,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "mexico",
+        "photo": "https://www.nationalgeographic.com/content/dam/travel/2016-digital/top-10-mexico-city/metropolitan-cathedral-zocalo-mexico-city.JPG",
+        "answers": [
+            2,
+            1,
+            1
+        ]
+    }
+]
+
+module.exports = vacations;
